8ba5a5941dd6f166eb2ce54133a7ab35ab1855ad
hungrybeagle <cyang@deltalearns.ca>
2025-01-13 09:54:49 -0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -48,6 +48,23 @@ class quadratic:
         # roots should be rounded to 2 decimal places
         pass
 
+    def axisOfSymmetry():
+        # requires no positional arguments
+        # will make use of class properties a,b and c 
+        # to determine the x value that is for the equation
+        # of the axis of symmetry
+        # should return the x value for the axis of symmetry
+        pass
+        return
+
+    def vertex()
+        # requires no positional arguments
+        # will make use of class properties a,b and c 
+        # to determine the x,y value of the vertex
+        # should return the a list with the x and y coordinates of the vertex
+        pass
+        return
+        
     def __init__():
         # this should require 3 positional arguments and assign the values
         # to self.a, self.b and self.c
@@ -61,21 +78,27 @@ if __name__ == "__main__":
     assert q1.hasRealRoots() == True
     assert q1.discriminant() == 0
     assert q1.roots == [-2,-2]
+    assert q1.axisOfSymmetry = -2
+    assert q1.vertex = [-2,0]
 
     q2 = quadratic(1,1,-6)
     assert q2.isFactorable() == True
     assert q2.hasRealRoots() == True
     assert q2.discriminant() == 25
     assert q2.roots == [-3,2]
+    assert q2.axisOfSymmetry = -0.5
+    assert q2.vertex = [-0.5,-6.25]
 
     q3 = quadratic(1,1,10)
     assert q3.isFactorable() == False
     assert q3.hasRealRoots() == False
     assert q3.discriminant() == -39
     assert q3.roots == []
+    assert q3.axisOfSymmetry = -0.5
 
     q4 = quadratic(1,10,1)
     assert q4.isFactorable() == False
     assert q4.hasRealRoots() == True
     assert q4.discriminant() == 96
     assert q4.roots == [-9.90,-0.10]
+    assert q4.axisOfSymmetry = -2.5
